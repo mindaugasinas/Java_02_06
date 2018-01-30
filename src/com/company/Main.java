abe0cb7901a916ef082c5c4ad695e899c3b5cf20
mindaugasinas <sinkeviciusm@gmail.com>
2018-01-30 15:53:40 +0200

    Sesta pamoka (projekto pradzia)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Iveskite savo ugi");
+
+        float Ugis = sc.nextFloat();
+
+        System.out.println("Iveskite savo svori");
+
+        float Svoris = sc.nextFloat();
+
+        float KMI = Svoris/(Ugis*Ugis);
+
+        System.out.println("Jusu KMI yra: " + KMI);
+
 	// write your code here
     }
 }
